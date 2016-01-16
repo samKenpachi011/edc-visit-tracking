3ab181aa8b18335b71f86f620cde21ca927c1dfa
erikvw <ew2789@gmail.com>
2016-01-16 11:56:39 +0200

    fix get_by_natural_key to use appointment only

--- edc_visit_tracking/managers.py
+++ edc_visit_tracking/managers.py
@@ -1,6 +1,3 @@
-import dateutil.parser
-
-from datetime import timedelta
 from django.db import models
 
 from edc_appointment.models import Appointment
@@ -8,13 +5,7 @@ from edc_appointment.models import Appointment
 
 class VisitManager(models.Manager):
 
-    def get_by_natural_key(self, report_datetime, visit_instance, visit_definition_code, subject_identifier_as_pk):
-        # deserialized date follows ECMA-262 specification which has less precision than that reported by mysql
-        if isinstance(report_datetime, basestring):
-            report_datetime = dateutil.parser.parse(report_datetime)
-        margin = timedelta(microseconds=999)
+    def get_by_natural_key(self, visit_instance, visit_definition_code, subject_identifier_as_pk):
         appointment = Appointment.objects.get_by_natural_key(
             visit_instance, visit_definition_code, subject_identifier_as_pk)
-        return self.get(
-            report_datetime__range=(report_datetime - margin, report_datetime + margin),
-            appointment=appointment)
+        return self.get(appointment=appointment)
