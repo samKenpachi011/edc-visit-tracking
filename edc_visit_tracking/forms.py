c59bd890c7ce2b824195b4f83f0ecff8585f4cb3
erikvw <ew2789@gmail.com>
2016-01-16 11:57:26 +0200

    add validation for report_datetime relative to consent or birth date

--- edc_visit_tracking/forms.py
+++ edc_visit_tracking/forms.py
@@ -28,18 +28,10 @@ class VisitFormMixin(object):
             raise forms.ValidationError('Appointment cannot be blank.')
 
     def validate_against_consent(self):
-        """Raises an exception if dates dont make sense with the consent.
-
-        Note: subjects like infants don't have a consent model so attribute "consent_model"
-        should be the birth model, just needs to have a dob."""
+        """Raise an exception if the report datetime doesn't make sense relative to the consent."""
         cleaned_data = self.cleaned_data
         appointment = cleaned_data.get('appointment')
-        try:
-            consent = self._meta.model.consent_model.objects.get(
-                registered_subject=appointment.registered_subject)
-        except ObjectDoesNotExist:
-            raise forms.ValidationError(
-                '\'{}\' does not exist for subject.'.format(self._meta.model.consent_model._meta.verbose_name))
+        consent = self.get_consent(appointment.registered_subject)
         try:
             if cleaned_data.get("report_datetime") < consent.consent_datetime:
                 raise forms.ValidationError("Report datetime cannot be before consent datetime")
@@ -48,7 +40,32 @@ class VisitFormMixin(object):
         if cleaned_data.get("report_datetime").date() < consent.dob:
             raise forms.ValidationError("Report datetime cannot be before DOB")
 
+    def get_consent(self, registered_subject):
+        """Return an instance of the consent model.
+
+        If no consent model is defined, as with infants, try for the birth_model."""
+        try:
+            consent = self._meta.model.consent_model.objects.get(
+                registered_subject=registered_subject)
+        except ObjectDoesNotExist:
+            raise forms.ValidationError(
+                '\'{}\' does not exist for subject.'.format(self._meta.model.consent_model._meta.verbose_name))
+        except AttributeError:
+            consent = self.get_birth_model_as_consent(registered_subject)
+        return consent
+
+    def get_birth_model_as_consent(self, registered_subject):
+        """Return the birth model in place of the consent_model."""
+        try:
+            birth_model = self._meta.model.birth_model.objects.get(
+                registered_subject=registered_subject)
+        except ObjectDoesNotExist:
+            raise forms.ValidationError(
+                '\'{}\' does not exist for subject.'.format(self._meta.model.consent_model._meta.verbose_name))
+        return birth_model
+
     def validate_time_point_status(self):
+        """Raise an exception if the timepioint is 'closed', see model method."""
         cleaned_data = self.cleaned_data
         appointment = cleaned_data.get('appointment')
         appointment.time_point_status_open_or_raise(exception_cls=forms.ValidationError)
@@ -65,23 +82,10 @@ class VisitFormMixin(object):
             if not cleaned_data.get('last_alive_date'):
                 raise forms.ValidationError(
                     'Provide date {participant} last known alive.'.format(participant=self.participant_label))
-#         if cleaned_data.get('survival_status') in [ALIVE, UNKNOWN]:
-#             try:
-#                 options = {
-#                     '{}__appointment__registered_subject'.format(self._meta.model.visit_model_attr):
-#                     cleaned_data.get(self._meta.model.visit_model_attr).appointment.registered_subject
-#                 }
-#                 death_report = self._meta.model.death_report_model.objects.get(**options)
-#                 if death_report.death_date < cleaned_data.get():
-#                     raise forms.ValidationError(
-#                         'Participant was reported deceased on {}. '
-#                         'Survival status cannot be {}'.format(
-#                             death_report.death_date.strftime('%Y-%m-%d'),
-#                             cleaned_data.get('survival_status')))
-#             except self._meta.model.death_report_model.DoesNotExist:
-#                 pass
 
     def validate_presence(self):
+        """Raise an exception if 'is_present' does not make sense relative to 'survival status',
+        'reason' and 'info_source'."""
         cleaned_data = self.cleaned_data
         if cleaned_data.get('is_present') == YES:
             if cleaned_data.get('survival_status') in [UNKNOWN, DEAD]:
@@ -102,6 +106,7 @@ class VisitFormMixin(object):
                         participant=self.participant_label))
 
     def validate_visit_reason_and_study_status(self):
+        """Raise an exception if reason is lost or completed and study status is not off study."""
         cleaned_data = self.cleaned_data
         if cleaned_data.get('reason') == LOST_VISIT:
             if cleaned_data.get('study_status') != OFF_STUDY:
