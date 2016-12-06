852fdc4eae524e2c68f3efb81411c0314ec5bb13
erikvw <ew2789@gmail.com>
2016-12-06 14:08:57 +0200

    add additional error handling

--- edc_visit_tracking/apps.py
+++ edc_visit_tracking/apps.py
@@ -4,9 +4,6 @@ from django.apps import AppConfig as DjangoAppConfig
 from django.core.exceptions import ImproperlyConfigured
 
 
-class EdcVisitTrackingAppConfigError(Exception):
-    pass
-
 ATTR = 0
 MODEL_LABEL = 1
 
@@ -33,19 +30,24 @@ class AppConfig(DjangoAppConfig):
         try:
             visit_model = django_apps.get_model(*self.visit_models[app_label][MODEL_LABEL].split('.'))
         except LookupError as e:
-            raise EdcVisitTrackingAppConfigError(
+            raise ImproperlyConfigured(
                 'Invalid visit model specified. See AppConfig for \'edc_visit_tracking\'. Got {}'.format(str(e)))
         return visit_model
 
     def visit_model_attr(self, label):
         """Return the attribute name for models that use the visit model for the given app_label."""
         app_label, model_name = label.split('.')
-        visit_model_attr = self.visit_models[app_label][ATTR]
+        try:
+            visit_model_attr = self.visit_models[app_label][ATTR]
+        except KeyError as e:
+            raise ImproperlyConfigured(
+                'Unable to select visit_model attr given \'{}\'. '
+                'Got {}. See \'edc_visit_tracking.AppConfig\'.'.format(label, str(e)))
         model = django_apps.get_model(app_label, model_name)
         try:
             getattr(model, visit_model_attr)
         except AttributeError as e:
-            raise EdcVisitTrackingAppConfigError(
+            raise ImproperlyConfigured(
                 'Invalid visit model attribute \'{}\' specified for model {}. '
                 'See AppConfig for \'edc_visit_tracking\'. Got {}'.format(visit_model_attr, label, str(e)))
         return visit_model_attr
