688ad4716b015f0d3133df1630b6354149395d29
erikvw <ew2789@gmail.com>
2016-12-20 14:25:20 +0200

    fix parameter list on get_by_natural_key

--- edc_visit_tracking/managers.py
+++ edc_visit_tracking/managers.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class CrfModelManager(models.Manager):
-    """A manager class for Crf models, models that have an FK to the visit tracking model."""
+    """A manager class for Crf models, models that have an FK to the visit model."""
 
-    def get_by_natural_key(self, visit_instance_number, code, subject_identifier_as_pk):
-        instance = self.model.visit_model.objects.get_by_natural_key(
-            visit_instance_number, code, subject_identifier_as_pk)
-        return self.get({self.model.visit_model_attr(): instance})
+    def get_by_natural_key(self, subject_identifier, visit_schedule_name, schedule_name, visit_code):
+        instance = self.model.visit_model().objects.get_by_natural_key(
+            subject_identifier, visit_schedule_name, schedule_name, visit_code)
+        return self.get(**{self.model.visit_model_attr(): instance})
 
     def get_for_visit(self, visit, **kwargs):
         """Returns an instance for the given visit."""
