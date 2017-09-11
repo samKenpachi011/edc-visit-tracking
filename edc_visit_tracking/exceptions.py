ff581a3aa9ac53b0e4a7433ff44393d46ca209f5
erikvw <ew2789@gmail.com>
2017-09-11 13:09:17 +0200

    move exception from models module

--- edc_visit_tracking/exceptions.py
+++ edc_visit_tracking/exceptions.py
@@ -2,3 +2,7 @@
 
 class VisitTrackingError(Exception):
     pass
+
+
+class PreviousVisitError(Exception):
+    pass
--- edc_visit_tracking/tests/test_visit_sequence.py
+++ edc_visit_tracking/tests/test_visit_sequence.py
@@ -3,8 +3,8 @@ from django.test import TestCase, tag
 from edc_appointment.models import Appointment
 from edc_base.utils import get_utcnow
 from edc_visit_schedule.site_visit_schedules import site_visit_schedules
-from edc_visit_tracking.constants import SCHEDULED
-from edc_visit_tracking.model_mixins import PreviousVisitError
+from ..constants import SCHEDULED
+from ..exceptions import PreviousVisitError
 
 from .models import SubjectVisit
 from .helper import Helper
