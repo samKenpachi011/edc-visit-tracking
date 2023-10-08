e7fe8d6011de517d0e3b2b951943d344dffb7ee6
samKenpachi011 <samuelkabelo1@gmail.com>
2023-10-08 08:21:10 +0200

    UPDATE: settings create keys true

--- edc_visit_tracking/settings.py
+++ edc_visit_tracking/settings.py
@@ -22,6 +22,7 @@ SITE_ID = 40
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
+DJANGO_AUTO_CREATE_KEYS = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ztj1y9k+4m@k(cut_mx6k&x&_*)-g0e#nfz=9)-#296j3*hbrq'
 
